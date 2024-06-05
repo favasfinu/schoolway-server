37b659afd1965e7edf79836198ff0ed904b082d2
finu <finufavasp@gmail.com>
2024-06-05 15:38:06 +0530

    message

--- server/models.py
+++ server/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Userprofile(models.model):
+    name=models.CharField(max_length=100)
+    bio=models.CharField(max_length=500)
+    image=models.ImageField(upload_to="user image")
+    date=models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
